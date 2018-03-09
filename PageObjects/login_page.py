4b5c2dfef0c7160dd9102b06fe3095ffb7a3220c
“zhouweisong” <zhouweisong@longbeach-inc.com>
2018-03-09 17:34:21 +0800

    新增两个用例

--- PageObjects/login_page.py
+++ PageObjects/login_page.py
@@ -18,19 +18,21 @@ from appium.webdriver.common.mobileby import MobileBy as By
 class Login_Page(BasePage):
 
     #手机号按钮
-    login_username_id = "com.jiemoapp:id/login_in_phone_number"
+    login_in_phone_number_icon = "com.jiemoapp:id/login_in_phone_number"
     #获取验证码按钮
-    verification_code_id = "com.jiemoapp:id/login_in_verification_code"
+    verification_code_icon = "com.jiemoapp:id/login_in_verification_code"
     #验证码输入框
-    login_in_verification_code_id ="com.jiemoapp:id/login_in_verification_code"
+    login_in_verification_code_box = "com.jiemoapp:id/login_in_verification_code"
     #切换成密码登录按钮/验证码登录按钮
-    password_login_in_id = "com.jiemoapp:id/password_login_in"
+    password_login_in_icon = "com.jiemoapp:id/password_login_in"
+    #获取验证码
+    fetch_verification_code_icon ="com.jiemoapp:id/fetch_verification_code"
     #登录按钮
-    new_login_in = "com.jiemoapp:id/new_login_in"
+    new_login_in_icon = "com.jiemoapp:id/new_login_in"
     #没有密码去注册按钮
-    have_no_phone_number = "com.jiemoapp:id/have_no_phone_number"
+    have_no_phone_number_icon = "com.jiemoapp:id/have_no_phone_number"
     #密码输入框
-    login_in_password = "com.jiemoapp:id/login_in_password"
+    login_in_password_box = "com.jiemoapp:id/login_in_password"
     #显示密码
     show_password_img = "com.jiemoapp:id/show_password_img"
     #【X】按钮
@@ -38,12 +40,20 @@ class Login_Page(BasePage):
 
 
 
+    #密码登录操作
+    def login_by_password_action(self, username, login_passwd):
+        self.find_element(By.ID, self.login_in_phone_number_icon).send_keys(username)
+        self.find_element(By.ID, self.password_login_in_icon).click()
+        self.find_element(By.ID, self.login_in_password_box).send_keys(login_passwd)
+        self.find_element(By.ID, self.new_login_in_icon).click()
+
     #验证码登录操作
-    def login_action(self, username,login_passwd):
-        self.find_element(By.ID,self.login_username_id).send_keys(username)
-        self.find_element(By.ID,self.password_login_in_id).click()
-        self.find_element(By.ID,self.login_in_password).send_keys(login_passwd)
-        self.find_element(By.ID,self.new_login_in).click()
+    def login_by_verification_action(self,username,login_verification):
+        self.find_element(By.ID, self.login_in_phone_number_icon).send_keys(username)
+        self.find_element(By.ID, self.fetch_verification_code_icon).click()
+        self.find_element(By.ID, self.login_in_verification_code_box).send_keys(login_verification)
+        self.find_element(By.ID, self.new_login_in_icon).click()
+
 
 
 
--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -25,27 +25,47 @@ from Common.logger2 import *
 class Test_Login:
 
     @pytest.mark.smoke
-    def test_login_ok(self,init_driver):
+    def test_login_by_password_ok(self, init_driver):
         home_page = Home_Page(init_driver)
         login_pag = Login_Page(init_driver)
-
         home_page.touch_login()
-        login_pag.login_action(CD.login_username,CD.login_passwd)
-        logging.info("测试用例：正常登陆芥末校园。")
+        login_pag.login_by_password_action(CD.login_username, CD.login_passwd)
+        logging.info("测试用例：密码登录----成功")
+        okname = home_page.get_ok_name()
+        logging.info("这个okname元素的text值 ：%s"%okname)
+        assert okname == '好'
+
+    def test_login_by_wrong_password(self, init_driver):
+        home_page = Home_Page(init_driver)
+        login_page = Login_Page(init_driver)
+        home_page.touch_login()
+        login_page.login_by_password_action(LD.login_success_username, LD.login_wrong_passwd)
+        logging.info("测试用例：密码登陆----密码错误")
+        message ='手机号或密码错误，请重新输入'
+        assert login_page.toast_exist(message)
+
+    @pytest.mark.debug
+    def test_login_by_verification_ok(self,init_driver):
+        home_page = Home_Page(init_driver)
+        login_pag = Login_Page(init_driver)
+        home_page.touch_login()
+        login_pag.login_by_verification_action(LD.login_success_username,LD.login_success_verification_code)
+        logging.info("测试用例：验证码登录----成功")
         okname = home_page.get_ok_name()
         logging.info("这个okname元素的text值 ：%s"%okname)
         assert okname == '好'
 
     @pytest.mark.debug
-    def test_login_wrong_password(self,init_driver):
+    def test_login_by_wrong_verification(self,init_driver):
         home_page = Home_Page(init_driver)
-        login_page = Login_Page(init_driver)
-
+        login_pag = Login_Page(init_driver)
         home_page.touch_login()
-        login_page.login_action(LD.login_success_username, LD.login_wrong_passwd)
-        logging.info("测试用例：登陆----密码错误")
-        message ='啦啦号或密码错误，请重新输入'
-        assert login_page.toast_exist(message)
+        login_pag.login_by_verification_action(LD.login_success_username,LD.login_wrong_verification_code)
+        logging.info("测试用例：验证码登录----验证码错误")
+        message ='验证码错误'
+        assert login_pag.toast_exist(message)
+
+
 
 
 
